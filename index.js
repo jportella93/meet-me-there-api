4e7d466a62d1db08e3fc922d39da422f94e9c899
jportella93 <jportella93@gmail.com>
2019-04-06 12:42:11 +0200

    feat: better error handling

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
+// Env variables
 require('dotenv').config();
-
-const Koa = require('koa');
-
-const app = new Koa();
-
-const logger = require('koa-logger');
-const bodyParser = require('koa-bodyparser'); // original
-const cors = require('koa-cors'); // original
-
-const router = require('./router');
-
 const PORT = Number(process.env.PORT) || 3000;
 
+// Dependencies
+const Koa = require('koa');
+const app = new Koa();
+
+// Routes
+const router = require('./router');
+
+// Middlewares
+const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser');
+const cors = require('koa-cors');
+
+const errorHandler = require('./middlewares/errorHandler')
+
+
 app
-  .use(cors())
+  .use(errorHandler)
   .use(logger())
+  .use(cors())
   .use(bodyParser())
   .use(router.routes())
   .use(router.allowedMethods());
@@ -24,6 +30,5 @@ const server = app.listen(
   PORT,
   console.log(`Meet Me There server listening on port ${PORT}`),
 )
-  .on('error', err => console.log(err));
 
 module.exports = server;
--- router.js
+++ router.js
@@ -5,20 +5,26 @@ const router = new Router();
 const flightsController = require('./flightsController')
 
 router.get('/flights', async (ctx, next) => {
-    let {dateFrom, dateTo, origins, destination} = ctx.query;
+  let {dateFrom, dateTo, origins, destination} = ctx.query;
 
-    if (!dateFrom | !dateTo | !origins) {
-      ctx.body = 'Error: missing parameters';
-      ctx.status = 500;
+  if (!dateFrom || !dateTo || !origins) {
+    const missingParams = [];
 
+    if (!dateFrom) missingParams.push('dateFrom');
+    if (!dateTo) missingParams.push('dateTo');
+    if (!origins) missingParams.push('origins');
+
+    const errorMessage = `Error: Missing parameters ${missingParams.join(', ')}.`;
+    ctx.throw(400, errorMessage);
+
+  } else {
+    origins = origins.split(',');
+
+    if (destination) {
+      ctx.body = await flightsController.getFlightsForOneDestination({dateFrom, dateTo, origins, destination})
     } else {
-      origins = origins.split(',');
-
-      if (destination) {
-        ctx.body = await flightsController.getFlightsForOneDestination({dateFrom, dateTo, origins, destination})
-      } else {
-        ctx.body = await flightsController.getFlights({dateFrom, dateTo, origins})
-      }
+      ctx.body = await flightsController.getFlights({dateFrom, dateTo, origins})
+    }
   }
 });
 
